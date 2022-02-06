fae21bf2b5b2293415113321e07dec0af0f93580
Shivam Dwivedi <shivam.dwivedi@neptune-ubi.com>
2022-02-06 13:32:36 +0530

    additional route config

--- packages/example-one/src/pages/index.route.ts
+++ packages/example-one/src/pages/index.route.ts
@@ -1,13 +1,25 @@
 import { Component, NgModule } from '@angular/core';
-import { Route } from '@angular/router';
+import { ActivatedRoute, Route } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 @Component({
-  template: `root`
+  template: `data: <div>{{data}}</div>`
 })
 export default class RouteComponent{
+  data: any;
+
+  constructor(route: ActivatedRoute) {
+    route.data.subscribe( d => {
+      this.data = (d['title']);
+    } );
+  }
+
+
 }
 
 @NgModule({
-  declarations: [RouteComponent]
+  declarations: [RouteComponent],
+  imports: [CommonModule]
 })
 class Module{}
 
