30dd3d16cb84e7e340c19514a5cb3dc182d66068
Shivam Dwivedi <shivam.dwivedi@neptune-ubi.com>
2022-02-07 23:02:14 +0530

    refactor code

--- packages/ng-rerouter/src/lib/routesGenerator.ts
+++ packages/ng-rerouter/src/lib/routesGenerator.ts
@@ -1,17 +1,4 @@
-import { Project, SourceFile, SyntaxKind, VariableDeclarationKind, Writers } from 'ts-morph';
-
-function isAdditionalConfigDefined(file: SourceFile) {
-  return file.getExportSymbols().some(exSymbol => exSymbol.getName() === 'routeConfig');
-}
-
-function capitalizeFirstLetter(string: string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function generateFileName(path: string, index: number) {
-  const segments = path.split("/");
-  return capitalizeFirstLetter(`${segments[segments.length - 1].replace(/[^0-9a-z]/gi, '')}_${index}`);
-}
+import { ArrayLiteralExpression, Project, SourceFile, SyntaxKind, VariableDeclarationKind, Writers } from 'ts-morph';
 
 /**
  * @param tsConfigFilePath
@@ -26,17 +13,74 @@ export function generateRoutes(
   const project = new Project({
     tsConfigFilePath: tsConfigFilePath,
   });
+  project.addSourceFilesAtPaths(pagesRootDirectory + '/**/*');
+
 
   const outputFile = project.createSourceFile(outputPath, undefined, {
     overwrite: true,
   });
-  project.addSourceFilesAtPaths(pagesRootDirectory + '/**/*');
-  const dir = project.getDirectoryOrThrow(pagesRootDirectory)!;
   outputFile.addImportDeclaration({
     moduleSpecifier: '@angular/router',
     namedImports: ['Routes'],
   });
+  const arrayLiteralExpression = createRoutesArray(outputFile);
+  const dir = project.getDirectoryOrThrow(pagesRootDirectory)!;
+  dir
+    .getDescendantSourceFiles()
+    .filter( file => file.getFilePath().endsWith(".route.ts"))
+    .sort((dir1, dir2) => dir2.getFilePath().localeCompare(dir1.getFilePath()))
+    .forEach((file, index) => {
+      const filePath = outputFile.getRelativePathAsModuleSpecifierTo(file);
+        const pathWithExt = dir.getRelativePathTo(file);
+        const path = pathWithExt.endsWith('index.route.ts')
+          ? pathWithExt.substring(0, pathWithExt.length - 'index.route.ts'.length -1 )
+          : pathWithExt.substring(0, pathWithExt.length - '.route.ts'.length);
+        const importSymbolName = generateFileName(path, index);
+        outputFile.addImportDeclaration({
+          moduleSpecifier: filePath,
+          defaultImport: importSymbolName,
+        });
+        arrayLiteralExpression.addElement(
+          Writers.object({
+            path: (writer) => writer.quote(path),
+            component: importSymbolName,
+          })
+        );
+        if(isAdditionalConfigDefined(file)){
+          spreadAdditionalRouteConfig(importSymbolName, outputFile, filePath, arrayLiteralExpression);
+        }
+    });
+  outputFile.save().catch((e) => console.error(e));
+}
 
+
+function isAdditionalConfigDefined(file: SourceFile) {
+  return file.getExportSymbols().some(exSymbol => exSymbol.getName() === 'routeConfig');
+}
+
+function capitalizeFirstLetter(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function generateFileName(path: string, index: number) {
+  const segments = path.split("/");
+  return capitalizeFirstLetter(`${segments[segments.length - 1].replace(/[^0-9a-z]/gi, '')}_${index}`);
+}
+
+function spreadAdditionalRouteConfig(importSymbolName: string, outputFile: SourceFile, filePath: string, arrayLiteralExpression: ArrayLiteralExpression) {
+  const routeConfigSymbol = `${importSymbolName}_routeConfig`;
+
+  outputFile.addImportDeclaration({
+    moduleSpecifier: filePath,
+    namedImports: [`routeConfig as ${routeConfigSymbol}`]
+  });
+
+  arrayLiteralExpression.getElements()[arrayLiteralExpression.getElements().length - 1]
+    .asKind(SyntaxKind.ObjectLiteralExpression)
+    ?.addSpreadAssignment({ expression: `${routeConfigSymbol}` });
+}
+
+function createRoutesArray(outputFile: SourceFile) {
   const arrayLiteralExpression = outputFile
     .addVariableStatement({
       declarationKind: VariableDeclarationKind.Const,
@@ -44,64 +88,16 @@ export function generateRoutes(
         {
           name: 'routes',
           initializer: '[ ]',
-          type: 'Routes',
-        },
-      ],
+          type: 'Routes'
+        }
+      ]
     })
     .getDeclarations()[0]
     .getInitializerIfKindOrThrow(SyntaxKind.ArrayLiteralExpression);
+
   outputFile.addExportDeclaration({
     namedExports: ['routes'],
   });
-  dir
-    .getDescendantSourceFiles()
-    .filter( file => file.getFilePath().endsWith(".route.ts"))
-    .sort((dir1, dir2) => dir2.getFilePath().localeCompare(dir1.getFilePath()))
-    .forEach((file, index) => {
-      const filePath = outputFile.getRelativePathAsModuleSpecifierTo(file);
-      if (filePath.length !== 0) {
-
-
-        const pathWithExt = dir.getRelativePathTo(file);
-
-        const path = pathWithExt.endsWith('index.route.ts')
-          ? pathWithExt.substring(0, pathWithExt.length - 'index.route.ts'.length -1 )
-          : pathWithExt.substring(0, pathWithExt.length - '.route.ts'.length);
-
-
-        console.log("Route found: " + file.getFilePath());
-
-        const importSymbolName = generateFileName(path, index);
-        outputFile.addImportDeclaration({
-          moduleSpecifier: filePath,
-          defaultImport: importSymbolName,
-        });
-
-        file.getExportDeclarations().map(ex => ex.getNamedExports()).forEach((nx) => {
-          console.log(nx.map(n => n.getName()));
-        });
-
-        arrayLiteralExpression.addElement(
-          Writers.object({
-            path: (writer) => writer.quote(path),
-            component: importSymbolName,
-          })
-        );
-
-        if(isAdditionalConfigDefined(file)){
-          const routeConfigSymbol = `${importSymbolName}_routeConfig`;
-
-          outputFile.addImportDeclaration({
-            moduleSpecifier: filePath,
-            namedImports: [`routeConfig as ${routeConfigSymbol}`]
-          })
-
-          arrayLiteralExpression.getElements()[arrayLiteralExpression.getElements().length -1]
-            .asKind(SyntaxKind.ObjectLiteralExpression)
-            ?.addSpreadAssignment({expression: `${routeConfigSymbol}`});
-        }
-      }
-    });
-  outputFile.save().catch((e) => console.error(e));
+  return arrayLiteralExpression;
 }
 
